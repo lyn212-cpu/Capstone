4d5ccf5347363243d67f6e98794856c066cc2087
Astrid004 <rhealyndeasis73@gmail.com>
2025-05-24 19:02:06 +0800

    PHP Script

---
+++ Backend/Admin Access/Display_Profile_Admin.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'connect.php';
+session_start();
+$email = $_SESSION['email'];
+
+$stmt = $conn->prepare("SELECT * FROM Users WHERE email = ? AND role IN ('Admin', 'Administrator')");
+$stmt->bind_param("s", $email);
+$stmt->execute();
+$data = $stmt->get_result()->fetch_assoc();
+
+echo json_encode($data);
+$stmt->close();
+$conn->close();
+?>
