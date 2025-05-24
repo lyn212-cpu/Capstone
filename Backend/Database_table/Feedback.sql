47d41311c19d6d6d7a6c5a4c2489d0c2ccf377ac
Astrid004 <rhealyndeasis73@gmail.com>
2025-05-24 13:31:19 +0800

    B

---
+++ Backend/Database_table/Feedback.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS Feedback (
+    feedback_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    course_id INT,
+    feedback TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (course_id) REFERENCES NC_Course(course_id) ON DELETE SET NULL
+)
