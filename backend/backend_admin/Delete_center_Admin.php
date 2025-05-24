ce99a7db044277afce556ab9bcc3dcb4ada184f1
Astrid004 <rhealyndeasis73@gmail.com>
2025-05-24 12:52:26 +0800

    Delete backend directory

--- backend/backend_admin/Delete_center_Admin.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'db_connect.php';
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id'])) {
-    $stmt = $pdo->prepare("DELETE FROM TrainingCenter_Admin WHERE id = ?");
-    $stmt->execute([$_POST['id']]);
-    echo "Training Center deleted successfully.";
-}
-?>
