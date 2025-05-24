dc54341d8b59fcb10036acf80bfe7d46f22a7627
Astrid004 <rhealyndeasis73@gmail.com>
2025-05-24 19:10:55 +0800

    PHP Script

---
+++ Backend/Admin Access/Delete_User.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'connect.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $user_id = $_POST['user_id'];
+
+    $stmt = $conn->prepare("DELETE FROM Users WHERE user_id = ?");
+    $stmt->bind_param("i", $user_id);
+
+    echo $stmt->execute() ? "User deleted." : "Error: " . $stmt->error;
+    $stmt->close();
+    $conn->close();
+}
+?>
