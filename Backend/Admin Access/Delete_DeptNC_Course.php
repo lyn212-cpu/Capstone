86a83a97aa0e9fd06fa662ce49d18660273fda15
Astrid004 <rhealyndeasis73@gmail.com>
2025-05-24 23:06:05 +0800

    PHP Script

---
+++ Backend/Admin Access/Delete_DeptNC_Course.php
@@ -0,0 +1,10 @@
+<?php
+include 'connect.php';
+
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $conn->query("DELETE FROM department_courses WHERE id=$id");
+}
+
+header("Location: index.php");
+?>
