01b297434cd8cb6058f25a62a64c66fc9007afdb
Jefte9898 <jefteramonjr@gmail.com>
2025-06-14 02:39:52 +0800

    Fix the eye icon for the password

--- UserInterface/Password_Sign_Up.js
+++ UserInterface/Password_Sign_Up.js
@@ -5,13 +5,15 @@ function togglePasswordVisibility() {
     const eyeSlash = document.getElementById('eyeSlash');
 
     if (passwordInput.type === 'password') {
+        // Show password
         passwordInput.type = 'text';
-        eyeOpen.classList.add('hidden');
-        eyeSlash.classList.remove('hidden');
+        eyeOpen.classList.remove('hidden');    // Show open eye
+        eyeSlash.classList.add('hidden');      // Hide slashed eye
     } else {
+        // Hide password
         passwordInput.type = 'password';
-        eyeOpen.classList.remove('hidden');
-        eyeSlash.classList.add('hidden');
+        eyeOpen.classList.add('hidden');       // Hide open eye
+        eyeSlash.classList.remove('hidden');   // Show slashed eye
     }
 }
 
--- UserInterface/Sign_Up.php
+++ UserInterface/Sign_Up.php
@@ -169,18 +169,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                         class="w-full text-black focus:outline-none" required />
 
                     <button type="button" id="togglePassword" class="absolute right-3">
-                        <!-- Eye Icon (show password) -->
-                        <svg id="eyeOpen" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                            class="size-6 text-black">
-                            <path d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
-                            <path fill-rule="evenodd"
-                                d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 0 1 0-1.113ZM17.25 12a5.25 5.25 0 1 1-10.5 0 5.25 5.25 0 0 1 10.5 0Z"
-                                clip-rule="evenodd" />
-                        </svg>
-
-                        <!-- Eye Slash Icon (hide password) -->
+                        <!-- Eye Slash Icon (password hidden, click to show) -->
                         <svg id="eyeSlash" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                            class="size-6 hidden text-black">
+                            class="size-6 text-black">
                             <path
                                 d="M3.53 2.47a.75.75 0 0 0-1.06 1.06l18 18a.75.75 0 1 0 1.06-1.06l-18-18ZM22.676 12.553a11.249 11.249 0 0 1-2.631 4.31l-3.099-3.099a5.25 5.25 0 0 0-6.71-6.71L7.759 4.577a11.217 11.217 0 0 1 4.242-.827c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113Z" />
                             <path
@@ -188,7 +179,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                             <path
                                 d="M6.75 12c0-.619.107-1.213.304-1.764l-3.1-3.1a11.25 11.25 0 0 0-2.63 4.31c-.12.362-.12.752 0 1.114 1.489 4.467 5.704 7.69 10.675 7.69 1.5 0 2.933-.294 4.242-.827l-2.477-2.477A5.25 5.25 0 0 1 6.75 12Z" />
                         </svg>
+
+                        <!-- Eye Icon (password visible, click to hide) -->
+                        <svg id="eyeOpen" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                            class="size-6 hidden text-black">
+                            <path d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
+                            <path fill-rule="evenodd"
+                                d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 0 1 0-1.113ZM17.25 12a5.25 5.25 0 1 1-10.5 0 5.25 5.25 0 0 1 10.5 0Z"
+                                clip-rule="evenodd" />
+                        </svg>
                     </button>
+
                 </div>
 
                 <!-- Sign In Button -->
